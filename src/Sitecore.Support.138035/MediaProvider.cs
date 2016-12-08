e3d517547cc69c48518c080abbc76c1874a0b407
Dmitry Kolinchuk <dmko@sitecore.net>
2016-12-08 11:53:05 +0200

    Render "Media Link" specified in the "General Link" field with "la" parameter depending on the context language

---
+++ src/Sitecore.Support.138035/MediaProvider.cs
@@ -0,0 +1,30 @@
+﻿using Sitecore.Configuration;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Globalization;
+using Sitecore.IO;
+using Sitecore.Links;
+using Sitecore.Resources.Media;
+using Sitecore.Web;
+using System;
+using System.Web;
+using System.Xml;
+
+namespace Sitecore.Support.Resources.Media
+{
+    public class MediaProvider : Sitecore.Resources.Media.MediaProvider
+    {
+        public override string GetMediaUrl(MediaItem item)
+        {
+            Assert.ArgumentNotNull(item, "item");
+            MediaUrlOptions options = MediaUrlOptions.Empty;
+            if (LinkManager.LanguageEmbedding != LanguageEmbedding.Never)
+            {
+                options.Language = Context.Language;
+                return this.GetMediaUrl(item, options);
+            }
+            return this.GetMediaUrl(item, options);
+        }
+    }
+}
